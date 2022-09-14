9f3b43919e619c053cc638ae39a41f64da70169e
Furkan <frknsprnl@hotmail.com>
2022-09-15 02:19:41 +0300

    course model/controller/route added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
+const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
 
 // MIDDLEWARES
 app.set("view engine", "ejs");
@@ -9,6 +11,7 @@ app.use(express.static("public"));
 
 // ROUTES
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
