db30c14d2579732aa562b3aab15665fa5e6b5f1c
Furkan <frknsprnl@hotmail.com>
2022-09-15 23:49:40 +0300

    courses route | getAllCourses added

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.route("/").get(pageController.getIndexPage);
 router.route("/about").get(pageController.getAboutPage);
-router.route("/courses").get(pageController.getCoursesPage);
 router.route("/dashboard").get(pageController.getDashboardPage);
 router.route("/contact").get(pageController.getContactPage);
 router.route("/login").get(pageController.getLoginPage);
