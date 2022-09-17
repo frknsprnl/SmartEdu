5ac9cb471fb6f9d788c3ef52fdae5e4accca86f3
Furkan <frknsprnl@hotmail.com>
2022-09-17 13:18:09 +0300

    register page & user model & user route & bcrypt pw hash added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 
 // MIDDLEWARES
 app.set("view engine", "ejs");
@@ -21,6 +22,7 @@ mongoose.connect("mongodb://localhost/smartedu-db").then(() => {
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 const port = 3000;
 app.listen(port, () => {
---
+++ controllers/authController.js
@@ -0,0 +1,16 @@
+const User = require("../models/User");
+
+exports.createUser = async (req, res) => {
+  const user = await User.create(req.body);
+  try {
+    res.status(201).json({
+      status: "success",
+      user,
+    });
+  } catch {
+    res.status(404).json({
+      status: "fail",
+      error,
+    });
+  }
+};
