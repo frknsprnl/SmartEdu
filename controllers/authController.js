bd13cee84604089b3b3f01fe3813f85347e97441
Furkan <frknsprnl@hotmail.com>
2022-09-20 02:40:53 +0300

    after register redirection set

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,7 @@ const bcrypt = require("bcrypt");
 exports.createUser = async (req, res) => {
   const user = await User.create(req.body);
   try {
-    res.status(201).json({
-      status: "success",
-      user,
-    });
+    res.status(201).redirect("/login");
   } catch (error) {
     res.status(404).json({
       status: "fail",
