87f97832710e77c7634f40767e8b83c4047fb172
Furkan <frknsprnl@hotmail.com>
2022-09-20 18:13:40 +0300

    Course - Teacher Relation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const Category = require("../models/Category");
+const Course = require("../models/Course");
 const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
@@ -47,10 +48,12 @@ exports.logoutUser = (req, res) => {
 exports.getDashboardPage = async (req, res) => {
   const user = await User.findOne({ _id: req.session.UserID });
   const categories = await Category.find({});
+  const courses = await Course.find({ user: req.session.UserID });
 
   res.status(200).render("dashboard", {
     page_name: "dashboard",
     user,
     categories,
+    courses,
   });
 };
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -2,7 +2,12 @@ const Category = require("../models/Category");
 const Course = require("../models/Course");
 
 exports.createCourse = async (req, res) => {
-  const course = await Course.create(req.body);
+  const course = await Course.create({
+    name: req.body.name,
+    description: req.body.description,
+    category: req.body.category,
+    user: req.session.UserID,
+  });
   try {
     res.status(201).redirect("/courses");
   } catch {
@@ -25,7 +30,9 @@ exports.getAllCourses = async (req, res) => {
       filter = { category: category._id };
     }
 
-    const courses = await Course.find(filter).sort("-createdAt");
+    const courses = await Course.find(filter)
+      .sort("-createdAt")
+      .populate("user");
 
     const categories = await Category.find();
 
@@ -44,7 +51,9 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
-    const course = await Course.findOne({ slug: req.params.slug });
+    const course = await Course.findOne({ slug: req.params.slug }).populate(
+      "user"
+    );
 
     res.status(200).render("course", {
       course,
