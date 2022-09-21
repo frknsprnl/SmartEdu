d37c547ebf9d167abc18760b48934cbe8b5d1c41
Furkan <frknsprnl@hotmail.com>
2022-09-21 23:58:41 +0300

    sendemail function added

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,4 +1,5 @@
-const User = require("../models/User");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 exports.getIndexPage = (req, res) => {
   res.status(200).render("index", {
@@ -34,4 +35,44 @@ exports.getRegisterPage = (req, res) => {
   res.status(200).render("register", {
     page_name: "register",
   });
+};
+
+exports.sendEmail = async (req, res) => {
+  const outputMessage = `
+  
+  <h1> Mail Details </h1>
+
+  <ul>
+    <li> Name: ${req.body.name} </li>
+    <li> Email: ${req.body.email} </li>
+  </ul>
+
+  <h1> Message </h1>
+  <p> ${req.body.message} </p>
+  
+  <code>This message is sent from SmartEDU.</code>
+  `;
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "streakswild@gmail.com", // gmail user
+      pass: process.env.GMAIL_PASS, // gmail password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"SmartEDU Contact 👻" <streakswild@gmail.com>', // sender address
+    to: "frknsprnl@hotmail.com", // list of receivers
+    subject: "Smart EDU Contact Form", // Subject line
+    html: outputMessage, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  res.status(200).redirect("/contact");
 };
